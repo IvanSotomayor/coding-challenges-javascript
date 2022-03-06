9f0f5e9ae03affa5f723c6b00b94dd3da995aa01
IvanSotomayor <63268327+IvanSotomayor@users.noreply.github.com>
2022-03-06 15:04:23 -0600

    Create solution1.js

---
+++ linkedLists/removeDuplicates/solution1.js
@@ -0,0 +1,26 @@
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function removeDuplicatesFromLinkedList(linkedList) {
+  // Write your code here.
+	let prev = linkedList
+	let current = linkedList.next
+	let actualValue = linkedList.value
+	
+	while(current){
+		if(current.value === actualValue){
+			prev.next = current.next
+			current.next = null
+			current = prev.next
+		}else{
+			prev = current
+			current = current.next
+			actualValue = prev.value
+		}
+	}
+  return linkedList;
+}
