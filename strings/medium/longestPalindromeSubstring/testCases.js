d7984fe004fe887ca6955b79be1d4324fb31f224
IvanSotomayor <63268327+IvanSotomayor@users.noreply.github.com>
2022-04-03 16:08:25 -0500

    Create testCases.js

---
+++ strings/medium/longestPalindromeSubstring/testCases.js
@@ -0,0 +1,144 @@
+Yay, your code passed all the test cases!
+
+14 / 14 test cases passed.
+
+Test Case 1 passed!
+Expected Output
+xyzzyx
+Your Code's Output
+xyzzyx
+View Outputs Side By Side
+Input(s)
+{
+  "string": "abaxyzzyxf"
+}
+Test Case 2 passed!
+Expected Output
+a
+Your Code's Output
+a
+View Outputs Side By Side
+Input(s)
+{
+  "string": "a"
+}
+Test Case 3 passed!
+Expected Output
+noon
+Your Code's Output
+noon
+View Outputs Side By Side
+Input(s)
+{
+  "string": "it's highnoon"
+}
+Test Case 4 passed!
+Expected Output
+noon
+Your Code's Output
+noon
+View Outputs Side By Side
+Input(s)
+{
+  "string": "noon high it is"
+}
+Test Case 5 passed!
+Expected Output
+abccba
+Your Code's Output
+abccba
+View Outputs Side By Side
+Input(s)
+{
+  "string": "abccbait's highnoon"
+}
+Test Case 6 passed!
+Expected Output
+zzzzzzzzzzzzzzzzzzzz
+Your Code's Output
+zzzzzzzzzzzzzzzzzzzz
+View Outputs Side By Side
+Input(s)
+{
+  "string": "abcdefgfedcbazzzzzzzzzzzzzzzzzzzz"
+}
+Test Case 7 passed!
+Expected Output
+abcdefgfedcba
+Your Code's Output
+abcdefgfedcba
+View Outputs Side By Side
+Input(s)
+{
+  "string": "abcdefgfedcba"
+}
+Test Case 8 passed!
+Expected Output
+aa
+Your Code's Output
+aa
+View Outputs Side By Side
+Input(s)
+{
+  "string": "abcdefghfedcbaa"
+}
+Test Case 9 passed!
+Expected Output
+abcdefggfedcba
+Your Code's Output
+abcdefggfedcba
+View Outputs Side By Side
+Input(s)
+{
+  "string": "abcdefggfedcba"
+}
+Test Case 10 passed!
+Expected Output
+zz2345abbbba5432zz
+Your Code's Output
+zz2345abbbba5432zz
+View Outputs Side By Side
+Input(s)
+{
+  "string": "zzzzzzz2345abbbba5432zzbbababa"
+}
+Test Case 11 passed!
+Expected Output
+5abbbba5
+Your Code's Output
+5abbbba5
+View Outputs Side By Side
+Input(s)
+{
+  "string": "z234a5abbbba54a32z"
+}
+Test Case 12 passed!
+Expected Output
+5abbba5
+Your Code's Output
+5abbba5
+View Outputs Side By Side
+Input(s)
+{
+  "string": "z234a5abbba54a32z"
+}
+Test Case 13 passed!
+Expected Output
+b12365456321b
+Your Code's Output
+b12365456321b
+View Outputs Side By Side
+Input(s)
+{
+  "string": "ab12365456321bb"
+}
+Test Case 14 passed!
+Expected Output
+aca
+Your Code's Output
+aca
+View Outputs Side By Side
+Input(s)
+{
+  "string": "aca"
+}
