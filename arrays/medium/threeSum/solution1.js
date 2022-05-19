e542a8d4f35e3c130c05289575edcbd267df5d09
IvanSotomayor <63268327+IvanSotomayor@users.noreply.github.com>
2022-05-18 20:05:01 -0500

    Update solution1.js

--- arrays/medium/threeSum/solution1.js
+++ arrays/medium/threeSum/solution1.js
@@ -1,1 +1,27 @@
-
+function threeNumberSum(array, targetSum) {
+	array.sort((a,b) => a - b);
+	let result = [];
+	
+	for(let i = 0; i < array.length - 2; i++){
+		let x = i;
+		let y = i + 1;
+		let z = array.length - 1;
+		
+		while(y < z){
+			if(array[x] + array[y] + array[z] == targetSum){
+				result.push([array[x], array[y], array[z]]);
+				y++;
+				z--;
+			}
+			else if(array[x] + array[y] + array[z] > targetSum){
+				z--;
+			}
+			else if(array[x] + array[y] + array[z] < targetSum){
+				y++;
+			}
+		}
+	}
+	
+	return result;
+	
+}
